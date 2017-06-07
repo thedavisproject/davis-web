0b94bd84d39115d2faecad4803f65832e30403e3
Joel Douglass <joel.douglass@velir.com>
2017-06-06 23:12:24 -0400

    Add testing/linting for CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-var gulp = require('gulp'),
-  tasks = require('davis-shared').build,
-  config = require('./gulp.config')(),
-  argv = require('yargs').argv;
+const gulp = require('gulp');
+const tasks = require('davis-shared').build;
+const config = require('./gulp.config')();
+const argv = require('yargs').argv;
 
-gulp.task('test', tasks.test(config.testFiles));
-gulp.task('lint', tasks.lint(config.allJs));
+gulp.task('test', tasks.test(config.testFiles, argv.ci));
+
+gulp.task('lint', tasks.lint(config.allJs, argv.ci));
+
 gulp.task('watch', function() {
   gulp.watch(config.allJs, ['lint', 'test']);
 });
+
 gulp.task('bump', tasks.bump(['./package.json'], argv.level || 'patch'));
+
 gulp.task('default', ['watch', 'lint', 'test']);
